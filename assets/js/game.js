fdc0b6f657d9ea572280916d27ffacbe40793ea0
EC2 Default User <ec2-user@ip-172-31-91-46.ec2.internal>
2019-10-19 20:38:05 +0000

    Game rules added in comment in js file

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+/* global $ */
+/*
+GAME FUNCTION:
+- Player must correctly guess all of the matching image pairs
+- Player has infinite guesses, but there is a time limit
+- Three different difficulties listed in a dropdown bar
+- Notify player of time left
+- Notify the player of the correct answer if there is a match
+- Let player choose to play again
+*/
