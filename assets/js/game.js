3a9a16c738a33639447722dbeacabadda0fc160f
EC2 Default User <ec2-user@ip-172-31-91-46.ec2.internal>
2019-10-19 21:14:14 +0000

    Dropdown bar added

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,3 +21,13 @@ let hasFlippedCard = false
 let boardLocked = false;
 let firstCard, secondCard;
 let selectedOneCard  = false;
+
+   <!-- Select Bar -->
+
+    <div class="select-bar">
+        <select id="dropdown-toggle" onchange="selectDifficulty()">Select Difficulty</button>
+       <option class="dropdown-item" value="Easy">Easy</option>
+       <option class="dropdown-item" value="Medium">Medium</option>
+       <option class="dropdown-item" value="Hard">Hard</option>
+    </select>
+    </div>
